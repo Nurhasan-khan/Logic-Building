79e788c67e6e3395f104d792aa91405845239539
Nurhasan <nkhan724412@gmail.com>
2025-11-11 18:49:30 +0530

    Day 9

--- logicbuilding/src/conditional_thinking/Seven.java
+++ logicbuilding/src/conditional_thinking/Seven.java
@@ -2,9 +2,17 @@ package conditional_thinking;
 
 public class Seven {
     public static void main(String[] args) {
+        System.out.println("Enter an Integer: ");
+        int number = new java.util.Scanner(System.in).nextInt();
+        System.out.println(multipleOrIsEnds(number));
 
     }
     private static String multipleOrIsEnds(int number){
-
+       if (number % 7 == 0 )
+           return "Multiple of Seven";
+       else if (number % 10 == 7)
+           return "Ends with Seven";
+      else
+          return "None";
     }
 }
