e4227d4f25480f0c138763cf415b25e135cf52c1
Nurhasan <nkhan724412@gmail.com>
2026-01-18 23:22:10 +0530

    Solved four patterns

---
+++ logicbuilding/src/looping_patterns/Patterns.java
@@ -0,0 +1,57 @@
+package looping_patterns;
+
+public class Patterns {
+    public static void main(String[] args) {
+        pattern4(5);
+
+    }
+
+    /**
+     *    *****
+     *    *****
+     *    *****
+     *    *****
+     *    *****
+     * @param n
+     */
+    private static void pattern1(int n){
+        for (int i = 1; i<=n; i++){
+            for(int j = 1; j<=n; j++)
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
+
+    /**
+     *      *
+     *      **
+     *      ***
+     *      ****
+     *      *****
+     * @param n
+     */
+    private static void pattern2(int n){
+        for (int i = 1; i<=n; i++){
+            for (int j = 1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    private static void pattern3(int n){
+        for (int i = 1; i<=n; i++){
+            for(int j = 1; j<=n-i+1; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    private static void pattern4(int n){
+        for (int row = 1; row<=n; row++){
+            for (int col = 1; col<=row; col++){
+                System.out.print(col+" ");
+            }
+            System.out.println();
+        }
+    }
+}
