2b67d6041a9b2610ef2748f2c97d715f736854ce
Nurhasan <nkhan724412@gmail.com>
2026-01-20 00:29:53 +0530

    Solved Three patterns

--- logicbuilding/src/looping_patterns/Patterns.java
+++ logicbuilding/src/looping_patterns/Patterns.java
@@ -1,10 +1,6 @@
 package looping_patterns;
 
 public class Patterns {
-    public static void main(String[] args) {
-        pattern4(5);
-
-    }
 
     /**
      *    *****
@@ -12,7 +8,6 @@ public class Patterns {
      *    *****
      *    *****
      *    *****
-     * @param n
      */
     private static void pattern1(int n){
         for (int i = 1; i<=n; i++){
@@ -28,7 +23,6 @@ public class Patterns {
      *      ***
      *      ****
      *      *****
-     * @param n
      */
     private static void pattern2(int n){
         for (int i = 1; i<=n; i++){
@@ -53,5 +47,35 @@ public class Patterns {
             }
             System.out.println();
         }
+    }
+    private static void pattern5(int n){
+        for (int i = 1; i<=2*n-1; i++){
+            int totalCols = i > n ? 2 * n - i : i;
+            for (int j = 1; j<=totalCols; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    private static void pattern6(int n){
+        for (int i = 1; i<=n; i++){
+            for (int j = 1; j<=i; j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern7(int n){
+        for (int i = 1; i<=n; i++){
+            for (int j = 1; j<=n-i+1; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        pattern7(5);
+
     }
 }
