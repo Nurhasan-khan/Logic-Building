47fb044e401417cbf9e8a0a2ace5f5151e2b4d35
Nurhasan <nkhan724412@gmail.com>
2026-01-21 22:46:04 +0530

    Nine Problem solved

--- logicbuilding/src/looping_patterns/Patterns.java
+++ logicbuilding/src/looping_patterns/Patterns.java
@@ -73,9 +73,130 @@ public class Patterns {
             System.out.println();
         }
     }
+    static void pattern8(int n){
+        for(int i = 1; i<=n; i++){
+            for (int j = 1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int k = 1; k<=2*i-1; k++){
+                System.out.print("*");
+            }
+            for (int j = 1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern9(int n){
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=i-1; j++){
+                System.out.print(" ");
+            }
+            for(int k = 1; k<=2*n-(2*i-1); k++){
+                System.out.print("*");
+            }
+
+            for(int j = 1; j<=i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern10(int n){
+        for (int i = 1; i<=n; i++){
+            for (int spaces = 1; spaces<=n-i; spaces++){
+                System.out.print(" ");
+            }
+            for (int stars = 1; stars<=i; stars++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern11(int n){
+        for (int i = 1; i<=n; i++){
+            for (int j = 1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int k = 1; k<=2*i-1; k++){
+                System.out.print("*");
+            }
+
+            for (int j = 1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=i-1; j++){
+                System.out.print(" ");
+            }
+            for(int k = 1; k<=2*n-(2*i-1); k++){
+                System.out.print("*");
+            }
+            for(int j = 1; j<=i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern12(int n){
+        for (int i = 1; i<=n; i++){
+            int start = i%2==0 ? 0 : 1;
+            for (int j=1; j<=i; j++){
+                System.out.print(start+" ");
+                start = 1 - start;
+            }
+            System.out.println();
+        }
+    }
+    static void pattern13(int n){
+        for (int i = 1; i<=n; i++){
+            for (int j = 1; j<=i; j++){
+                System.out.print(j);
+            }
+            for (int spaces = 1; spaces<=2*(n-i); spaces++){
+                System.out.print(" ");
+            }
+
+            for (int j = i; j>=1; j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    static void pattern14(int n){
+        int initialNumber = 1;
+        for (int i = 1; i<=n; i++){
+            for (int j = 1; j<=i; j++){
+                System.out.print(initialNumber+" ");
+                initialNumber += 1;
+            }
+            System.out.println();
+        }
+    }
+    static void pattern15(int n){
+        for (int i = 1; i<=n; i++){
+//            char alpha = 'A';
+            for(char c = 'A'; c<'A'+i; c++){
+                System.out.print(c+" ");
+//                alpha += 1;
+            }
+            System.out.println();
+        }
+    }
+    static void pattern16(int n){
+        for (int i = 1; i<=n; i++){
+            for (char c = 'A'; c<'A'+n-i+1; c++){
+                System.out.print(c+" ");
+            }
+            System.out.println();
+        }
+    }
+
 
     public static void main(String[] args) {
-        pattern7(5);
+        pattern16(5);
 
     }
 }
