d01c0b79c657b8cb52ae2b67f594e80f7c53edfe
Nurhasan <nkhan724412@gmail.com>
2026-04-28 22:15:04 +0530

    Optimal Solution Added

--- logicbuilding/src/arrays/SetMatrixZero.java
+++ logicbuilding/src/arrays/SetMatrixZero.java
@@ -3,9 +3,10 @@ package arrays;
 public class SetMatrixZero {
     public static void main(String[] args) {
         int[][] matrix = {
-                {1,1,1},
-                {1,0,1},
-                {1,1,1}
+                {1,1,1,1},
+                {1,0,1,1},
+                {1,1,0,1},
+                {0,1,1,1}
         };
         System.out.println("Before: ");
         for (int[] i : matrix){
@@ -14,7 +15,8 @@ public class SetMatrixZero {
             System.out.println();
         }
 //        setZeroBrute(matrix);
-        setZerosBetter(matrix);
+//        setZerosBetter(matrix);
+        setZerosOptimal(matrix);
         System.out.println("After: ");
         for (int[] i : matrix){
             for (int j : i)
@@ -35,6 +37,32 @@ public class SetMatrixZero {
             for (int j = 0; j<matrix[i].length; j++){
                 if (matrix[i][j] == -1)
                     matrix[i][j] = 0;
+            }
+        }
+    }
+    static void setZerosOptimal(int[][] matrix){
+        int cols = 1;
+        for (int i = 0; i<matrix.length; i++){
+            for (int j = 0; j<matrix[i].length; j++){
+                if (matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    if (j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        cols = 0;
+                }
+            }
+        }
+        for (int i = matrix.length-1; i>=0; i--){
+            for (int j = matrix[i].length-1; j>=0; j--){
+                if(j != 0) {
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }else{
+                    if (cols == 0)
+                        matrix[i][0] = 0;
+                }
             }
         }
     }
