d5ebfcef19be63462a95220835c885d1a5fb772b
Nurhasan khan <nkhan724412@gmail.com>
2026-08-01 09:20:19 +0530

    Count Number of 1s

---
+++ logicbuilding/src/basic/maths/bitmanipulation/NumberOfOneBits.java
@@ -0,0 +1,12 @@
+package basic.maths.bitmanipulation;
+
+class NumberOfOneBits {
+    public int hammingWeight(int n) {
+        int count = 0;
+        for (int i = 0; i<32; i++){
+            if(((n>>i)&1) == 1)
+                count++;
+        }
+        return count;
+    }
+}
