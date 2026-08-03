bfad5dd6c6ff6d5bd874761f6590bcbebc955f7e
Nurhasan khan <nkhan724412@gmail.com>
2026-08-03 09:47:59 +0530

    Roman To Integer

---
+++ logicbuilding/src/strings/RomanToInteger.java
@@ -0,0 +1,25 @@
+package strings;
+
+import java.util.Map;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        System.out.println(romanToInt("LVIII"));
+    }
+    static int romanToInt(String s){
+        Map<Character,Integer> map = Map.of(
+                'I',1,'V',5,'X',10,
+                'L',50,'C',100,'D',500,'M',1000
+        );
+        int res = 0;
+        for(int i = 0; i<s.length(); i++){
+            int current = map.get(s.charAt(i));
+            int next = (i + 1 < s.length() ? map.get(s.charAt(i+1)) : 0);
+            if(current < next)
+                res -= current;
+            else
+                res += current;
+        }
+        return res;
+    }
+}
