b0717c76b57e7ce54b536ea4253824a94be8cd35
Nurhasan khan <nkhan724412@gmail.com>
2026-08-10 20:16:04 +0530

    Sieve Of Eratosthenes

--- logicbuilding/src/basic/maths/Prime.java
+++ logicbuilding/src/basic/maths/Prime.java
@@ -2,7 +2,11 @@ package basic.maths;
 
 public class Prime {
     public static void main(String[] args) {
-        System.out.println(isPrime(4));
+        System.out.println(isPrime(3));
+//        primeTillN(40);
+        int n = 40;
+        boolean[] primes = new boolean[n+1];
+        allPrimes(n,primes);
     }
     static boolean isPrime(int n){
         int count = 0;
@@ -14,5 +18,26 @@ public class Prime {
             }
         }
         return count == 2;
+    }
+    static void primeTillN(int n){
+        for (int i = 2; i<n; i++){
+            System.out.println(i+" : "+isPrime(i));
+        }
+    }
+
+    // sieve of Eratosthenes
+    static void allPrimes(int n , boolean[]primes){
+        for (int i = 2; i*i<=n; i++){
+            if (!primes[i]){
+                for (int j = i*2; j<=n; j+=i){
+                    primes[j] = true;
+                }
+            }
+        }
+        for (int i = 2; i<=n; i++){
+            if(!primes[i]) {
+                System.out.print(i + ",");
+            }
+        }
     }
 }
